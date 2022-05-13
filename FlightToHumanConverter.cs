c46f329fa3949e8310c660ae81989650a84a47c4
AliaksandrBialchytski <73606879+AliaksandrBialchytski@users.noreply.github.com>
2022-05-13 14:28:43 +0200

    add project

---
+++ FlightToHumanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF16_Bind
+{
+    public class FlightToHuman : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            string c = $"{((Crew)value).Name}" + $"{((Crew)value).Surname}";
+            return c;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WPF-DataBinding
 WPF (C#) project
+
+This project was created during 4th semester
